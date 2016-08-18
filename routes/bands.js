251ec99fadc9878dee6a861f413c9227dac6af6e
niran manandhar <nirnaism@gmail.com>
2016-08-18 17:29:53 -0500

    added delete route

--- routes/bands.js
+++ routes/bands.js
@@ -40,5 +40,14 @@ router.get('/:bandId', (req, res, next) => {
   })
 });
 
+router.delete('/:bandId', (req, res, next) => {
+  Band.findById(req.params.bandId).remove(function (err) {
+    if (err) {
+      res.status(500).send()
+    } else {
+      res.status(204).send()
+    }
+  })
+});
 
 module.exports = router;
