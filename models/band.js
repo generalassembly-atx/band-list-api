a439375fcd7fb40383ce1b2a4b1e36b6e635818d
niran manandhar <nirnaism@gmail.com>
2016-08-18 16:46:52 -0500

    testing model

--- models/band.js
+++ models/band.js
@@ -1,2 +1,13 @@
 // TODO: Add and export band model
 // attrs: name, genre, corruptedByTheSystem
+const mongoose = require('mongoose');
+const bandSchema = {
+  name: String,
+  geners: String,
+  curruptedByTheSystem: Boolean
+  }
+
+
+const band = mongoose.model('Band', bandSche);
+
+module.exports = Band;
