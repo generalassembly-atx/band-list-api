d5c6dbffc012bf5697163b8a5ca2e627787e4632
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-08-18 16:40:52 -0500

    setup

--- models/band.js
+++ models/band.js
@@ -1,2 +1,14 @@
 // TODO: Add and export band model
 // attrs: name, genre, corruptedByTheSystem
+
+const mongoose = require('mongoose');
+
+var todoSchema = {
+  task: String,
+  completed: { type:Boolean, default:false},
+  userId: String
+}
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
