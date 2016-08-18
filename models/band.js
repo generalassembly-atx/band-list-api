46976ec41c66fe9f71fb0c7f6f6a12a2803958e3
niran manandhar <nirnaism@gmail.com>
2016-08-18 17:15:35 -0500

    set use route

--- models/band.js
+++ models/band.js
@@ -8,6 +8,6 @@ const bandSchema = {
   }
 
 
-const band = mongoose.model('Band', bandSche);
+const Band = mongoose.model('Band', bandSchema);
 
 module.exports = Band;
--- routes/bands.js
+++ routes/bands.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var band = require('../models/band');
+const _ = require('lodash');
 
+router.use('/', (req, res, next) => {
+  req.body = _.pick(req.body, ['name', 'genre', 'corruptedByTheSystem'])
+ next()
+});
 
 
 module.exports = router;
