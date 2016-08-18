0ec9a749322ee3fc1fe8fe47f63851023e6443ba
Shirish Dali <designsbydali@gmail.com>
2016-08-18 16:53:03 -0500

    GET route created

--- models/band.js
+++ models/band.js
@@ -1,2 +1,14 @@
 // TODO: Add and export band model
 // attrs: name, genre, corruptedByTheSystem
+const mongoose = require('mongoose');
+
+var bandSchema = {
+  name: String,
+  genre: String,
+  corruptedByTheSystem: {type: Boolean, default: false},
+}
+
+const Band = mongoose.model('Band', bandSchema);
+
+
+module.exports = Band;
--- routes/bands.js
+++ routes/bands.js
@@ -2,5 +2,51 @@ var express = require('express');
 var router = express.Router();
 
 
+var Band = require('../models/band');
+var _ = require('lodash');
+
+//MIDDLEWARE FUNCTION
+router.use(function (req, res, next) {
+  req.body = _.pick(req.body, ['name', 'genre', 'corruptedByTheSystem'])
+
+  next()
+})
+
+//GET band
+router.get('/', function (req, res, next) {
+  Band.find({}, function (err, bands) {
+    if (err) {
+      res.status(500).send()
+    } else {
+      res.json(bands)
+    }
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
