370ea962ab39f5aa1ed0d6910c80133410e915f8
jesse houghton <OiseauNoir>
2016-08-18 21:02:53 -0500

    added lodash

--- routes/bands.js
+++ routes/bands.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var Band = require('../models/band')
+const _ = require('lodash');
 
-
-router.get('/', function(req, res, next) {
-});
+router.use(function (req, res, next) {
+  req.body = _.pick(req.body, ['name', 'genre', 'corruptedByTheSystem'])
+  next()
+})
 
 module.exports = router;
