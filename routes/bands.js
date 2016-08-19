63a14eeac703d44a31c6b533916248bc7960d9f0
Chris Robbins <Jaguar85>
2016-08-18 21:42:30 -0500

    DELETE /band/:id

--- routes/bands.js
+++ routes/bands.js
@@ -40,7 +40,15 @@ router.get('/:bandId', (req, res, next) => {
     }
   })
 })
-
+router.delete('/:bandId', (req, res, next) => {
+  Band.findById(req.params.bandId).remove((err) => {
+    if (err) {
+      res.status(500).send()
+    } else {
+      res.status(204).send()
+    }
+  })
+})
 
 
 
