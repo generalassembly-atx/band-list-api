4b5effb7d6162391346e3fac40a4e586e0b8af41
Chris Robbins <Jaguar85>
2016-08-18 20:38:49 -0500

    get route

--- routes/bands.js
+++ routes/bands.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var Band = require('../models/band')
+const _ = require('lodash');
+
+router.use((req, res, next) => {
+  req.body = _.pick(req.body, ['name', 'genre', 'corruptedByTheSystem'])
+  next()
+})
+
+router.get('/', (req, res, next) => {
+  Band.find({}, (err, students) => {
+    if (err) {
+      res.status(500).send()
+    } else {
+      res.json(bands)
+    }
+  })
+});
+
+
+
+
+
+
+
+
 
 
 
