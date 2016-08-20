229d85605393b61b421060b83c9466e40dd550cb
jesse houghton <OiseauNoir>
2016-08-20 16:31:15 -0500

    final commit

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//add cors
+//add cors package
 app.use(cors());
 
 app.use('/', routes);
