c2c358393247a083f17487438852c73455b7fff2
Nathan Jensby <njensby@gmail.com>
2016-10-13 14:29:12 -0500

    added GETcollection and POST routes

--- routes/bands.js
+++ routes/bands.js
@@ -2,6 +2,36 @@ var express = require('express');
 var router = express.Router();
 const _ = require('lodash');
 
+var Band = require('../models/band');
 
+//GET /bands collection
+router.get('/', function (req, res) {
+  Band.find({}, function (err, bands) {
+    if (err) {
+      res.status(500).send();
+    } else {
+      res.json(bands);
+    }
+  })
+})
+
+//GET /bands/:bandName
+
+//POST /bands
+router.post('/', function (req, res) {
+  var band = new Band(req.body);
+  band.save(function (err) {
+    if (err) {
+      res.status(500).send();
+    } else {
+      res.json(band);
+    }
+  })
+})
+
+
+//PUT /:bandName
+
+//DELETE /bands/:bandName
 
 module.exports = router;
