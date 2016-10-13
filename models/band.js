c68ca3d0bfc9ee92ee4d48742a37db8a13ea7685
David Leseten <dleseten@gmail.com>
2016-10-13 14:15:02 -0500

    Band model created.

--- models/band.js
+++ models/band.js
@@ -1,2 +1,11 @@
 // TODO: Add and export band model
 // attrs: name, genre, corruptedByTheSystem
+const mongoose = require('mongoose');
+const bandSchema = {
+  bandName: String,
+  genre: String,
+  rating: Number,
+  isCool: Boolean
+}
+const Band = mongoose.model("Band", bandSchema);
+module.exports = Band;
