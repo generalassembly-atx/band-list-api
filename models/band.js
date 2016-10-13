3fac414e6694ef83c15441ba60f0a8a5e0ebca65
Nathan Jensby <njensby@gmail.com>
2016-10-13 14:17:22 -0500

    built band.js model and created .env

--- models/band.js
+++ models/band.js
@@ -1,2 +1,14 @@
-// TODO: Add and export band model
+// : Add and export band model
 // attrs: name, genre, corruptedByTheSystem
+
+const mongoose = require('mongoose');
+
+const bandSchema = {
+  bandName: String,
+  genre: String,
+  numAlbums: Number,
+  isSellOut: Boolean
+}
+
+const Band = mongoose.model('Band', bandSchema);
+module.exports = Band;
