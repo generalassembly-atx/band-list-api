2844ee19dc72056b2dd2831c74d11ca0dc9cfc06
Joseph Callaway <jeauxcal@gmail.com>
2016-10-13 15:20:59 -0500

    Added new post router

--- routes/bands.js
+++ routes/bands.js
@@ -4,9 +4,15 @@ var _ = require('lodash');
 
 var Band = require('../models/band');
 
-module.exports = router;
 
-router.get('/', function (res, res, next) {
+
+router.use(function (req, res, next) {
+  next()
+})
+
+// STEP 1. CREATE INITIAL '/' ROUTE
+
+router.get('/', function (req, res, next) {
   Band.find({}, function (err, bands) {
     if(err){
       res.status(500).send();
@@ -14,4 +20,23 @@ router.get('/', function (res, res, next) {
       res.json(bands);
     }
   })
+});
+
+
+//STEP 2. CREATE POST ROUTE
+router.post('/',function (req, res, next) {
+  var band = new Band(req.body);
+  band.save(function (err) {
+    if(err){
+      res.status(500).send();
+    }else {
+      res.json(band);
+    }
+  })
+});
+
+router.get('/bands', function (req, res, next) {
+  Band.find()
 })
+
+module.exports = router;
