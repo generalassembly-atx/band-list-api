041613a5f3a6ff0e7063513abfa6f717a8e03705
Joseph Callaway <jeauxcal@gmail.com>
2016-10-13 15:43:47 -0500

    Added use route to find band & added put route to update

--- routes/bands.js
+++ routes/bands.js
@@ -5,13 +5,13 @@ var _ = require('lodash');
 var Band = require('../models/band');
 
 
-
+// STEP 4. CREATE MIDDLEWARE
 router.use(function (req, res, next) {
-  next()
+  req.body = _.pick(req.body, ['corruptedByTheSystem'])
+  next();
 })
 
 // STEP 1. CREATE INITIAL '/' ROUTE
-
 router.get('/', function (req, res, next) {
   Band.find({}, function (err, bands) {
     if(err){
@@ -35,39 +35,49 @@ router.post('/',function (req, res, next) {
   })
 });
 
+
+/*---------------------------------------------------------------------------*/
+
 /// STEP 3. POST INSIDE REST CLIENT TO TEST '/'
 
   ///ONCE THAT IS DONE, CREATE GET ROUTE FOR INPUTTED DATA///
 
 
-// STEP 4. CREATE PUT ROUTE
+/// STEP 4 IS AT THE TOP!!! ADDING MIDDLEWARE!!!
 
+/*---------------------------------------------------------------------------*/
+
+
+// STEP 5. CREATE USE ROUTE
+
+router.use('/:id', function (req, res, next) {
+  Band.findOne(function (err) {
+    if(err){
+      res.status(500).send();
+    }else if (!band){
+      res.status(404).send();
+    } else{
+      res.band = band;
+    }
+  })
+});
+
+// STEP 6. CREATE PUT "UPDATE ATTRIBUTE" ROUTE
 router.put('/:id', function (req, res, next) {
   var updatedBand = Object.assign(res.band, req.body)
   updatedBand.save(function (err) {
     if(err){
       res.status(500).send();
-    }else {
-      res.json(band);
     }
   })
 });
 
 
-// STEP . CREATE USE ROUTE
 
-router.use('/', function (req, res, next) {
-  Band.findOne(), function (err) {
-    if(err){
-      res.status(500).send();
-    }else {
-      res.json(band);
-    }
-  }
-})
 
 router.get('/bands', function (req, res, next) {
-  Band.find()
+  res.json(band);
 })
+
 
 module.exports = router;
