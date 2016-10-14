4e4ecb07194b3e2d5876ffc6db9569affd38c9b3
jordan wade <jordanwade2009@live.com>
2016-10-13 21:40:48 -0500

    almost got the song part

--- models/band.js
+++ models/band.js
@@ -1,2 +1,16 @@
 // TODO: Add and export band model
 // attrs: name, genre, corruptedByTheSystem
+const mongoose = require('mongoose');
+
+var bandSchema = {
+  name: String,
+  genre: String,
+  curruptedByTheSystem: Boolean,
+  songs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song'}]
+}
+
+
+
+var Band = mongoose.model('Band', bandSchema);
+
+module.exports = Band;
